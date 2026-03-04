8fe5df62d63f2fe01dbd1774f3ff152de62ed731
WillianSavio <Williansavio98@gmail.com>
2026-03-04 20:05:55 -0300

    Desafio_dictionary

--- Desafio_dictionary.py
+++ Desafio_dictionary.py
@@ -1,19 +1,31 @@
 
 print(" === CADASTRE O PRODUTO === ")
 
-produto = {
+lista_produto = []
+
+while True:
+
+    produto = {
     "Nome": "",
     "Preco": "",
     "Quantidade em estoque": "",
     "Categoria": ""
-}
+    }
 
-produto["Nome"] = input("Qual o nome do poduto que deseja cadastrar? \n" )
-produto["Preco"] = input("Qual o preço do produto? \n")
-produto["Quantidade em estoque"] = input("Qual a quantidade em estoque? \n")
-produto["Categoria"] = input("Qual a categoria do produto? \n")
+    lista_produto.append(produto)
 
-print("\n Produto cadastrado com sucesso")
+    produto["Nome"] = input("Qual o nome do poduto que deseja cadastrar? \n" )
+    produto["Preco"] = input("Qual o preço do produto? \n")
+    produto["Quantidade em estoque"] = input("Qual a quantidade em estoque? \n")
+    produto["Categoria"] = input("Qual a categoria do produto? \n")
 
-print(f"\n Nome: {produto["Nome"]}\n Preço: {produto["Preco"]}\n Estoque: {produto["Quantidade em estoque"]}\n Categoria: {produto["Categoria"]}\n")
+    print("\n Produto cadastrado com sucesso")
 
+    print(f"\n Nome: {produto["Nome"]}\n Preço: {produto["Preco"]}\n Estoque: {produto["Quantidade em estoque"]}\n Categoria: {produto["Categoria"]}\n")
+
+    pergunta = input("Você deseja adicionar mais produtos? Digite S ou N")
+
+    if pergunta.upper() != "S":
+        print("Encerrando o cadastro...")
+        print(lista_produto)
+        break
